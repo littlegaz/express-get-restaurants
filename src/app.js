bda5a218806cca1df6ae04a844f6059abdd71791
littlegaz <gareth.t.horne@hotmail.co.uk>
2024-05-20 11:04:39 +0100

    day 1

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
-
-
+app.get("/resturants", async (request, response) => {
+    const resturants = await Resturant.findAll({});
+    response.json(resturants);
+})
 
 
 module.exports = app;
